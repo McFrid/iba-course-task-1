bb558461be8625d3ba9aa7518ab634795db87b8d
Denis Urban <freezex69@gmail.com>
2019-09-21 23:37:24 +0300

    change get-request

--- http-get/src/main/java/by/iba/HttpController.java
+++ http-get/src/main/java/by/iba/HttpController.java
@@ -1,13 +1,14 @@
 package by.iba;
 
+
 class HttpController {
 
     static class HttpResponse {
-        int statusCode;
-        String message;
+        int statusCode = 200;
+        String message = " [{ body: 'f', title: 'b', userId: 'b', id: 'd' }]";
     }
 
     HttpResponse get(String url) {
-        return null;
+        return new HttpResponse();
     }
 }
