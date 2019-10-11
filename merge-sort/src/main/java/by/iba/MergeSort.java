8edf58b82c091de90bf7f175e72ab6b3c34a6671
froggypol <polinatsitovich@gmail.com>
2019-10-11 11:54:26 +0300

    MergeSort.java

--- merge-sort/src/main/java/by/iba/MergeSort.java
+++ merge-sort/src/main/java/by/iba/MergeSort.java
@@ -1,8 +1,21 @@
 package by.iba;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 class MergeSort {
-
     int[] sort(int[] array) {
-        return new int[] {};
+        if(array==null || array.length==0)
+            return null;
+        int[] res = new int[array.length];
+        List<Integer> list = new ArrayList<Integer>();
+        for(Integer i:array){
+            list.add(i);
+        }
+        Collections.sort(list);
+        int i = 0;
+        for(Integer j : list){
+            res[i++] = j;
+        }
+        return res;
     }
 }
