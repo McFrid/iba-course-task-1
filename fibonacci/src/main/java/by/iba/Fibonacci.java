e82cacbab5487a01f6a63df4e73d8a8779a9414a
Razumova Maria <7379165@gmail.com>
2019-10-11 11:54:32 +0300

    fibonacci task done

--- fibonacci/src/main/java/by/iba/Fibonacci.java
+++ fibonacci/src/main/java/by/iba/Fibonacci.java
@@ -2,7 +2,11 @@ package by.iba;
 
 class Fibonacci {
 
-    long F(int order) {
-        return -1;
+    long F(float order) {
+        if(order == 0)
+            return 0;
+        if(Math.abs(order) < 3)
+            return (long)Math.signum(order);
+        return F(order-Math.signum(order)) + F(order-2*Math.signum(order));
     }
 }
